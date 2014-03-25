2c765264edb173e5e286ae135dcbcd3de941392a
Teresa F <tere.fds@hotmail.com>
2014-03-25 20:42:34 -0300

    updating heuristicOne

--- deeptriptp/src/main/java/deeptrip/ai/heuristics/HeuristicOne.java
+++ deeptriptp/src/main/java/deeptrip/ai/heuristics/HeuristicOne.java
@@ -1,5 +1,6 @@
 package deeptrip.ai.heuristics;
 
+import deeptrip.ai.states.DeeptripAIState;
 import gps.api.GPSState;
 
 public class HeuristicOne implements Heuristic {
@@ -7,9 +8,15 @@ public class HeuristicOne implements Heuristic {
 	
 	@Override
 	public Integer getHValue(GPSState stateFrom, GPSState goalState) {
+		if(! (stateFrom instanceof DeeptripAIState)){
+			throw new IllegalArgumentException();
+			
+		}
 		if(stateFrom.compare(goalState)){
 			return 0;
 		}
+		DeeptripAIState state=(DeeptripAIState)stateFrom;
+		
 		
 		// TODO falta implementar de manera posta
 		return 1;
