bee96fa71ed655e4d450826140c2906d473a30d0
Teresa F <tere.fds@hotmail.com>
2014-04-22 20:40:16 -0300

    updating

--- neuralNetwork/multiLayerPerceptron.m
+++ neuralNetwork/multiLayerPerceptron.m
@@ -4,9 +4,28 @@
 %n factor de aprendizaje (por ahora es fijo pero esto va a cambiar)
 %patterns matriz de p x (e+1) (donde p es la cantidad de patrones y e es la cantidad de entradas SIN contar el umbral, y en la columna e+1-esima esta la salida deseada) 
 
-function multiLayerPerceptron(weightsHiddenLayer,weightsLastLayer,n,patterns,g,derivate,correctWeights)
+function multiLayerPerceptron(weights,n,patterns,g,derivate,epsilon,epoques)
+    E=1;
+    iterations=0;
+    totalLayers=size(weights)(1);
+   do
     
     
+    
+    
+    E=0;
+    for k=1:size(patterns)(1)
+       pattern=[-1, patterns(k,1:end-1)];
+        aux=weights{1,1}*pattern';
+        for j=1:totalLayers
+         aux=wights{j,1}*aux';
+        endfor
+        
+        
+    endfor    
+   while(E>epsilon && iterations<=epoques )
+   
+
    for i=1:size(patterns)(1) 
     pattern=[-1, patterns(i,1:end-1)];
     wishedOutput=patterns(i,end);
@@ -16,7 +35,7 @@ function multiLayerPerceptron(weightsHiddenLayer,weightsLastLayer,n,patterns,g,d
     sigma=arrayfun(g,hM);
     
     deltaoutput=(arrayfun(derivate,sigma))*(wishedOutput-sigma);
-    deltaHidden=(arrayfun(derivate,V1));
+    deltaHidden=(arrayfun(derivate,V1))*();
 
    endfor
     
