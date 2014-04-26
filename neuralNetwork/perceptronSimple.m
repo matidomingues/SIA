24c0b032f1bff89ccd1b7b63e878e0bfade6d77c
Facundo Martinez <fnmartinez88@gmail.com>
2014-04-26 12:24:47 -0300

    Mejora sutil en el perceptrón simple

--- neuralNetwork/perceptronSimple.m
+++ neuralNetwork/perceptronSimple.m
@@ -1,30 +1,33 @@
 #weigths es el vector con los pesos (w1 hasta wr) de las conexiones (donde w1 es el umbral
 #n es el factor de aprendizaje
 #patterns es una matriz con los patrones (en donde la primer columna representa el umbral, que es todo -1, 
-#y la última columna es el resultado, y las del medio son las distintas entradas)
+#y la ï¿½ltima columna es el resultado, y las del medio son las distintas entradas)
 
 #g es la funcion de activacion
 #La funcion perceptronSimple devuelve un vector con los pesos actualizados
 
 
 function tableAns= perceptronSimple(weigths,n,patterns,g,correctWeights)
-	tableAns=weigths;
+    tableAns=weigths;
 	inputs=length(patterns(1,:))-1;  #cantidad de entradas
-  patternsQty=size(patterns)(1); #cantidad de patrones
-	it=zeros(1,patternsQty);
-  while(sum(it)/length(it)<1)  
+    patternsQty=size(patterns)(1); #cantidad de patrones
   
-    i=floor(rand(1)*(patternsQty)+1);
-    
-	   x=patterns(i,1:inputs)*(tableAns)';
-		if (g(x)!=patterns(i,end))
-		   tableAns=correctWeights(tableAns,n,x,patterns(i,:));
-        it=zeros(1,patternsQty);
-    else
-        it(i)=1;
-    endif
-   end
-  
-     
-    sum(it)/length(it)
-endfunction
+    do
+        patternsOrder = randperm(patternsQty);
+        for i = 1:patternsQty
+          x = patterns(patternsOrder(i), 1:inputs) * (tableAns)';
+          while (g(x) ~= patterns(patternsOrder(i), end)) 
+            tableAns = correctWeights(tableAns, n, x, patterns(patternsOrder(i), :));
+            x = patterns(patternsOrder(i), 1:inputs) * (tableAns)';
+          end
+        end
+        learnt = true;
+        for i = 1:patternsQty
+          x = patterns(patternsOrder(i), 1:inputs) * (tableAns)';
+          if (g(x) ~= patterns(1, end)) 
+            learnt = false;
+            break;
+          end
+        end
+    until (learnt)
+ endfunction
