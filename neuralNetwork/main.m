a04ba47795e8da08304b8d83c756a2d554b0f437
Facundo Martinez <fnmartinez88@gmail.com>
2014-04-27 23:46:28 -0300

    Commenting!

--- neuralNetwork/main.m
+++ neuralNetwork/main.m
@@ -1,3 +1,12 @@
+% main function to use our multilayer perceptron
+% Arguments are:
+% 	inputFileName: Filename for the input file in ASCII containing the patterns. It should contain the inputs and the
+%		expected outputs in the mentioned order.
+%	trainingSetQty: The number of patterns from the file to be used as a training set.
+%	architecture: An array expressing the number of perceptron for each hidden layer.
+%	n: The learning factor.
+%	epsilon: The expected error.
+%	epoques: The number of epochs to be run.
 function main(inputFileName, trainingSetQty, arquitecture, expectedOutputs, n, epsilon, epoques)
 	patterns = load(inputFileName, '-ascii');
 	patternsQty = size(patterns);
