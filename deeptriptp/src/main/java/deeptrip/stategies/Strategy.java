6cd80d4353eab9abd2e71d4991986145a03ab395
Facundo Martinez <fnmartinez88@gmail.com>
2014-04-28 15:02:35 -0300

    Mudanza 2!

--- deeptriptp/src/main/java/deeptrip/stategies/Strategy.java
+++
@@ -1,7 +0,0 @@
-package deeptrip.stategies;
-
-import deeptrip.game.Board;
-
-public interface Strategy {
-	public Board execute(Board board);
-}
--- gps/src/main/java/gps/exception/SolutionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package gps.exception;
-
-public class SolutionNotFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
