a211f9e06b0daece7bc0e9c1b221bb67e9818187
Teresa F <tere.fds@hotmail.com>
2014-05-18 00:42:21 -0300

    changes

---
+++ genetics/src/main/java/ar/edu/itba/sia/genetics/selectors/impl/EliteUniversalFenotypeSelector.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.sia.genetics.selectors.impl;
+
+import java.util.Set;
+
+import ar.edu.itba.sia.genetics.fenotypes.Fenotype;
+import ar.edu.itba.sia.genetics.selectors.FenotypeSelector;
+
+public class EliteUniversalFenotypeSelector implements FenotypeSelector{
+
+	public Set<Fenotype> select(Set<Fenotype> fenotypes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
